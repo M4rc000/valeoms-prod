c9688af60cb22809ae76fef3998429f4b763a13d
Marco Antonio <marcoantoniomadgaskar@gmail.com>
2024-06-17 01:46:40 +0700

    0.0.22

--- material_list/material_list/application/views/master/print_material_list.php
+++
@@ -1,9 +0,0 @@
-<?php if (!empty($materials)): ?>
-  <ul>
-    <?php foreach ($materials as $material): ?>
-      <li><?php echo $material->name; // Replace 'name' with the actual column name ?></li>
-    <?php endforeach; ?>
-  </ul>
-<?php else: ?>
-  <p>No active materials found.</p>
-<?php endif; ?>
